38296bc94d966b71e7ba212f60a34b81bbb7ec4d
jjtillma <james.tillma@mines.sdsmt.edu>
2014-11-08 02:47:31 -0700

    Added some basic place holder classes

---
+++ CharacterSheetManager/Models/Ability.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterSheetManager.Models
+{
+    class Ability
+    {
+        private byte _score;
+        public byte Score
+        {
+            set { _score = value; }
+            get { return _score; }
+        }
+
+        private byte _modifier;
+        public byte Score
+        {
+            set { _modifier = value; }
+            get { return _modifier; }
+        }
+        private byte _bonus;
+        public byte Score
+        {
+            set { _bonus = value; }
+            get { return _bonus; }
+        }
+        private byte _temporaryModifier;
+
+        public byte TemporaryModifier
+        {
+            set { _temporaryModifier = value; }
+            get { return _temporaryModifier; }
+        }
+
+        private byte _temporaryScore;
+        public byte TemporaryScore
+        {
+            set { _temporaryScore = value; }
+            get { return _temporaryScore; }
+        }
+    }
+}
---
+++ CharacterSheetManager/Models/BasicCharacteristics.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterSheetManager.Models
+{
+    public class BasicCharacteristics
+    {
+
+        private byte _spellResistance;
+        public byte SpellResistance
+        {
+            set { _spellResistance = value; }
+            get { return _spellResistance; }
+        }
+
+        private byte _sizeModifier;
+        public byte SizeModifier
+        {
+            set { _sizeModifier = value; }
+            get { return _sizeModifier; }
+        }
+
+        private byte _grappleBonus;
+        public byte GrappleBonus
+        {
+            set { _grappleBonus = value; }
+            get { return _grappleBonus; }
+        }
+        private byte _grappleModifier;
+        public byte GrappleModifier
+        {
+            set { _grappleModifier = value; }
+            get { return (byte)(_grappleModifier + _sizeModifier + _grappleBonus); }
+        }
+
+        private byte _baseAttackBonus;
+        public byte BaseAttackBonus
+        {
+            set { _baseAttackBonus = value; }
+            get { return _baseAttackBonus; }
+        }
+    }
+}
