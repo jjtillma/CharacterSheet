2ba314592a370da79fbc3927a8189c10e23cb29f
silenced40 <adam.desenfants@mines.sdsmt.edu>
2014-12-19 01:33:02 -0700

    Added datagrid to armor list.

--- CharacterSheetManager/ViewModels/WeaponsAndArmor.cs
+++ CharacterSheetManager/ViewModels/WeaponsAndArmor.cs
@@ -4,13 +4,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Data;
 
 namespace CharacterSheetManager.ViewModels
 {
     public class WeaponsAndArmor : ViewModelBase
     {
+        public ICollectionView Weapons { get; private set; }
 
-        public List<Weapon> Weapons;
         public List<ProtectiveItem> Armor;
+
+        public WeaponsAndArmor()
+        {
+            var _weapons = new List<Weapon> { 
+                        new Weapon { Name = "Short Sword", DamageRoll = "1d6" },
+                        new Weapon { Name = "Long Sword", DamageRoll = "1d8" },
+                        new Weapon { Name = "Great Sword", DamageRoll = "1d10" }
+                        };
+
+
+            Weapons = CollectionViewSource.GetDefaultView(_weapons);
+        }
     }
 }
